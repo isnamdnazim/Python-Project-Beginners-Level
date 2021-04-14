eba35be26e2656057eb214e2ddeddc8d6a15166f
Md Nazim Uddin <56835478+isnamdnazim@users.noreply.github.com>
2021-04-15 02:13:41 +0600

    Add files via upload

---
+++ Hangman/5.Hangman.py
@@ -0,0 +1,60 @@
+import random
+from words import words
+import string
+
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)    #letter in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()      #what the user has guessed
+
+    lives = 6
+
+    # getting user input
+    while len(word_letters) > 0 and lives > 0:
+        # letters used
+        # ' '.join(['a', 'b', 'c']) --> ' a b c d'
+
+        print('You have', lives,' lives left and You have used these letters: ', ' '.join(used_letters))
+
+        # what current word is (ie W - R D)
+
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
+
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet  - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+            else: 
+                lives = lives - 1 # takes away a life if wrong
+                print('\n Your letter, ',user_letter ,'is not in word.')
+
+        elif user_letter in used_letters:
+            print('\nYou have already used the character. Please try again.')
+
+        else:
+            print('\nInvalid character. Please try again.')
+
+    # gets hare when len(word_letters) == 0 or when lives == 0
+    if lives == 0:
+        print('You died, sorry. The woed was',word)
+    else:
+        print('You guessed the word', word, '!!')
+
+
+if __name__ == '__main__':
+    hangman()
